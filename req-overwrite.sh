313939e6518c4569229defc5eab212166c7048da
Francesco Rolando <ogekuri@gmail.com>
2026-02-14 09:40:26 +0100

    new(core): add common scripts.

---
+++ req-overwrite.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Assegna il primo argomento passato allo script alla variabile FOLDER_PATH
+FOLDER_PATH="$1"
+
+# Controlla se la variabile è vuota (l'utente non ha passato argomenti)
+if [ -z "$FOLDER_PATH" ]; then
+    echo "Usage: $0 <path_to_folder>"
+    exit 1
+fi
+
+# Controlla se il percorso NON esiste o NON è una directory
+if [ ! -d "$FOLDER_PATH" ]; then
+    echo "Path does not exist"
+    exit 1
+else
+    # make dirs
+    mkdir -p "${FOLDER_PATH}/req"
+    mkdir -p "${FOLDER_PATH}/tech"
+    mkdir -p "${FOLDER_PATH}/docs"
+    mkdir -p "${FOLDER_PATH}/tests"
+    mkdir -p "${FOLDER_PATH}/src"
+    mkdir -p ".github/workflows"
+
+    #echo req --base "${FOLDER_PATH}" --req-dir "${FOLDER_PATH}/req" --tech-dir "${FOLDER_PATH}/tech" --doc-dir "${FOLDER_PATH}/docs" --test-dir "${FOLDER_PATH}/tests" --src-dir "${FOLDER_PATH}/src" --src-dir ".github/workflows" --overwrite-tech --enable-claude --enable-codex --enable-gemini --enable-github --enable-kiro --enable-opencode --enable-models --enable-tools --legacy
+    req --base "${FOLDER_PATH}" --req-dir "${FOLDER_PATH}/req" --tech-dir "${FOLDER_PATH}/tech" --doc-dir "${FOLDER_PATH}/docs" --test-dir "${FOLDER_PATH}/tests" --src-dir "${FOLDER_PATH}/src" --src-dir ".github/workflows" --overwrite-tech --enable-claude --enable-codex --enable-gemini --enable-github --enable-kiro --enable-opencode --enable-models --enable-tools --legacy
+fi
---
+++ req-update-preserve.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Assegna il primo argomento passato allo script alla variabile FOLDER_PATH
+FOLDER_PATH="$1"
+
+# Controlla se la variabile è vuota (l'utente non ha passato argomenti)
+if [ -z "$FOLDER_PATH" ]; then
+    echo "Usage: $0 <path_to_folder>"
+    exit 1
+fi
+
+# Controlla se il percorso NON esiste o NON è una directory
+if [ ! -d "$FOLDER_PATH" ]; then
+    echo "Path does not exist"
+    exit 1
+else
+    # make dirs
+    mkdir -p "${FOLDER_PATH}/req"
+    mkdir -p "${FOLDER_PATH}/tech"
+    mkdir -p "${FOLDER_PATH}/docs"
+    mkdir -p "${FOLDER_PATH}/tests"
+    mkdir -p "${FOLDER_PATH}/src"
+    mkdir -p ".github/workflows"
+
+    #echo req --update --preserve-models --base "${FOLDER_PATH}" --enable-claude --enable-codex --enable-gemini --enable-github --enable-kiro --enable-opencode --enable-models --enable-tools --legacy
+    req --update --preserve-models --base "${FOLDER_PATH}" --enable-claude --enable-codex --enable-gemini --enable-github --enable-kiro --enable-opencode --enable-models --enable-tools --legacy
+fi
